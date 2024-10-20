b7a938211970a20391a600b912cccb77961a4ab0
Aglaialianou <cs05268@uoi.gr>
2024-10-20 18:22:14 +0300

    Completed lab02!

--- lab02.s
+++ lab02.s
@@ -14,5 +14,16 @@ prog:
 # Do not remove the prog label or write code above it!
 #-----------------------------
 done:
+findLast_forward_withPointers:
+    add s0, zero, zero #initialize s0 = 0
+loop:
+    beq a1, zero, done1
+    lw t1, 0(a0)
+    bne t1, a2, next
+    add s0, a0, zero
+next:
+    addi a0, a0, 4 #go to next element
+    addi a1, a1, -1 #sub 1 from register a1(counter)
+done1:   
     addi a7, zero, 10 
     ecall
